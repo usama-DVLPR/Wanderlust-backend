2b8c780036a30d7834b102a3c3a7ed5d00503f47
Usama Hameed <usamahameed034@gmail.com>
2024-07-26 17:27:40 +0500

    create simple tour schema and model

--- server.js
+++ server.js
@@ -13,6 +13,26 @@ mongoose
     console.error("Database connection error:", err);
   });
 
+// schema
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    require: [true, "A tour must have a name"],
+    unique: true,
+  },
+  price: {
+    type: Number,
+    require: [true, "A tour must have a price"],
+  },
+  rating: {
+    type: Number,
+    default: 5,
+  },
+});
+
+// creating model from schema
+const TourModel = new mongoose.model("Tour", tourSchema);
+
 // server
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
