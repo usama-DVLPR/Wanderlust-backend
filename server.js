9827f0b63dc66df1c74147f261db1120fd992a38
Usama Hameed <usamahameed034@gmail.com>
2024-07-26 17:19:35 +0500

    connect mongodb with nodejs

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
+const DB = process.env.DATABASE;
+mongoose
+  .connect(DB, {})
+  .then(() => {
+    console.log("Database connection successful");
+  })
+  .catch((err) => {
+    console.error("Database connection error:", err);
+  });
 
 // server
 const port = process.env.PORT || 3000;
