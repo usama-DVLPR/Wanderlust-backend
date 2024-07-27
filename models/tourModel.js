a3c4de7287e3420db5bbc54e6b6c905e62ab4dbe
Usama Hameed <usamahameed034@gmail.com>
2024-07-27 17:21:06 +0500

    update tour model

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,15 +6,52 @@ const tourSchema = new mongoose.Schema({
     type: String,
     required: [true, "A tour must have a name"],
     unique: true,
+    trime: true,
+  },
+  duration: {
+    type: Number,
+    required: [true, "A tour must have a duration"],
+  },
+  maxGroupSize: {
+    type: Number,
+    required: [true, "A tour must have group size"],
+  },
+  difficulty: {
+    type: String,
+    required: [true, "A tour must have difficulty"],
   },
   price: {
     type: Number,
     required: [true, "A tour must have a price"],
   },
-  rating: {
+  ratingsAverage: {
     type: Number,
     default: 5,
   },
+  ratingsQuantity: {
+    type: Number,
+    default: 0,
+  },
+  priceDiscount: Number,
+  summary: {
+    type: String,
+    required: [true, "A tour must have description"],
+    trim: true,
+  },
+  description: {
+    type: String,
+    trim: true,
+  },
+  imageCover: {
+    type: String,
+    required: [true, "A tour must have cover image"],
+  },
+  images: [String],
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  startDates: [Date],
 });
 
 // creating model from schema
