7e6c4cba49a881220240ec4e575625df88783203
Usama Hameed <usama.hameed@linentech.net>
2024-08-31 17:26:39 +0500

    pagination implemeneted

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -33,6 +33,22 @@ exports.getAllTours = async (req, res, next) => {
         query=query.select('-__v')
     }
 
+    // pagination
+    const page=req.query.page * 1 || 1;
+    const limit =req.query.limit *1 || 100;
+    const skip= (page-1)*limit ;
+    query=query.skip(skip).limit(limit);
+
+    if(req.query.page){
+        const numTours=await Tour.countDocument;
+        console.log(numTours );
+        if(skip => numTours){
+            throw new Error("This page does not exist");
+
+        }
+
+    }
+
     const tours = await query;
     res.status(200).json({
       status: "success",
