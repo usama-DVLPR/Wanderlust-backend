8035bba30505bf68ee75fd4d0be4a0ef49afaf6e
Usama Hameed <usama.hameed@linentech.net>
2024-09-07 22:22:20 +0500

    getMonthaly plan routes using getMonthlyPlan

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -126,3 +126,99 @@ exports.getTourStats = async (req, res) => {
     });
   }
 };
+
+
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = +req.params.year; // 2021
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates",
+      },
+      {
+        $addFields: {
+          startDates: {
+            $dateFromString: {
+              dateString: {
+                $substr: ["$startDates", 0, 10],
+              },
+              format: "%Y-%m-%d",
+            },
+          },
+        },
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+        _id:{$month:'$startDates'},
+        numTourStarts:{$sum: 1},
+        tours:{
+          $push:'$name'
+        }
+      }
+    },
+    // {
+    //   $addFields:{
+    //     month:'$_id'
+    //   }
+    // },
+    {
+      $addFields: {
+        month: {
+          $arrayElemAt: [
+            [
+              "",
+              "January",
+              "February",
+              "March",
+              "April",
+              "May",
+              "June",
+              "July",
+              "August",
+              "September",
+              "October",
+              "November",
+              "December",
+            ],
+            "$_id",
+          ],
+        },
+      },
+    },
+    {
+      $project:{
+        _id:0
+      }
+    },
+    {
+      $sort:{numTourStarts:-1}
+    },
+    // {
+    //   $limit:6
+    // }
+    ]);
+
+    res.status(200).json({
+      status: "success",
+      results: plan.length,
+      data: {
+        plan,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      status: "fail",
+      message: error,
+    });
+  }
+};
