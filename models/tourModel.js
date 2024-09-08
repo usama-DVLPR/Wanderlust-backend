1b765cfe6d8e6514b61eba65301dffcb0aa31781
Usama Hameed <usama.hameed@linentech.net>
2024-09-08 17:32:21 +0500

    doc middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const slugify=require("slugify");
 // schema
 const tourSchema = new mongoose.Schema({
   name: {
@@ -8,6 +8,7 @@ const tourSchema = new mongoose.Schema({
     unique: true,
     trime: true,
   },
+  slug:String,
   duration: {
     type: Number,
     required: [true, "A tour must have a duration"],
@@ -63,6 +64,19 @@ const tourSchema = new mongoose.Schema({
 tourSchema.virtual('durationWeeks').get(function(){
   return this.duration/7
 })
+
+// Document Middleware  (run before .save(),.create() command)
+tourSchema.pre('save', function(next){
+  this.slug=slugify(this.name,{lower:true})
+  next()
+})
+
+// tourSchema.post('save',function(doc,next){
+//   console.log(doc);
+  
+//   next()
+// })
+
 
 // creating model from schema
 const Tour = new mongoose.model("Tour", tourSchema);
