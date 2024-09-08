f9d9edb51269d38be51f87fd5bcfacbe9c7b496d
Usama Hameed <usama.hameed@linentech.net>
2024-09-08 18:07:41 +0500

    Aggregate middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -89,7 +89,12 @@ tourSchema.pre(/^find/,function(next){
 })
 
 
+// Aggregate pipeline 
 
+tourSchema.pre('aggregate',function(next){
+  this.pipeline().unshift({$match :{secretTour:{$ne:true} }})
+  next()
+})
 // creating model from schema
 const Tour = new mongoose.model("Tour", tourSchema);
 module.exports = Tour;
