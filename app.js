6a173986de0d9961b10dd710dd9eb9ec1cba1699
Usama Hameed <usama.hameed@linentech.net>
2024-09-17 16:39:05 +0500

    handel not found route

--- app.js
+++ app.js
@@ -14,5 +14,12 @@ app.use(express.json());
 // routes
 app.use("/api/tours", tourRouter);
 app.use("/api/users", userRouter);
+app.all("*",(req,res,next)=>{
+  res.status(404).json({
+    status:'fail',
+    data:null,
+    message:`Can't find ${req.originalUrl} on this server!`
+  })
+})
 
 module.exports = app;
